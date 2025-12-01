427bd1f79bf577cf7f1cf58e01a9f663af3adb44
ahmedmuhammed14 <123456789+ahmedmuhammed1624@gmail.com.noreply.github.com>
2025-12-01 18:50:01 +0300

    feat: Add new static assets and user profile model, remove obsolete CSS/package files and user update template, and modify base template and forms.

--- base/forms.py
+++ base/forms.py
@@ -1,12 +1,12 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, User
+from .models import Room, User, Profile
 
 
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['name', 'username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 
 class RoomForm(ModelForm):
@@ -19,4 +19,15 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['avatar', 'name', 'username', 'email', 'bio']
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        for fieldname in self.fields:
+            self.fields[fieldname].help_text = None
